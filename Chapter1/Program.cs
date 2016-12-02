e1725ecc43279ba0a7fd9591b1440f1fe8354858
JackChe <chehaoj@gmail.com>
2016-12-02 17:33:16 +0800

    csharper Async Example

---
+++ Chapter1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("starting...");
+            //DoSomethingAsync();
+            Deadlock();
+            
+            Console.WriteLine("stoped...");
+            Console.ReadLine();
+        }
+
+        public static async Task DoSomethingAsync()
+        {
+            int val = 13;
+
+            await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+            val *= 2;
+
+            await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+
+            Console.WriteLine(val);
+        }
+
+        public static async Task WaitAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            
+        }
+
+        public static async void Deadlock()
+        {
+            Task task = WaitAsync();
+
+            task.Wait();
+        }
+
+      
+    }
+}
